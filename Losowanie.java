cd4841cb633ed92b428976f1a65e6c3498135f26
gjuliag <45037132+gjuliag@users.noreply.github.com>
2018-11-15 20:04:23 +0100

    Add files via upload

--- Losowanie.java
+++ Losowanie.java
@@ -4,52 +4,46 @@ package seriousgame;
 import java.util.Random;
 import static seriousgame.Parametry.*;
 
+
 public class Losowanie {
     
-    public static void losujSlowa(int terazPoziom){
+    public static void losujSlowa(){
         
         ladujSlowa();
         Random r= new Random();
         
-        los1= new String [3][3];
-        los2= new String [3][4];
-        los3= new String [3][5];
-        int x;//zmienna z przypisana wylosowana liczba
-        int[] spr1;//tablica do sprawdzania, czy zostala wylosowana juz dana litera (poziom1)
-        spr1= new int[3];
-        int[] spr2;//tablica do sprawdzania, czy zostala wylosowana juz dana litera (poziom2)
-        spr2= new int[4];
-        int[] spr3;//tablica do sprawdzania, czy zostala wylosowana juz dana litera (poziom3)
-        spr3= new int[5];
+        int d;//zmienna z przypisana wylosowana liczba
         
+        
+        for(int terazPoziom=1; terazPoziom<4; terazPoziom++){
         switch (terazPoziom) {
             case 1:
                 {
-                    for (int i=0; i<poziom1.length;i++){
+                        for (int i=0; i<poziom1.length;i++){
                         spr1[0]=0;
                         spr1[1]=0;
                         spr1[2]=0;
                         int licznik =0;
                         for(int j=0; j<poziom1[i].length; j++){
-                            x=r.nextInt(los1[i].length);
+                            d=r.nextInt(los1[i].length);
                              boolean q = false;
                             if (licznik != 3)
                                 q = true;
 
                             while(q){
-                                    if(spr1[x]==0) {
-                                        los1[i][x]= poziom1[i][j];
-                                        spr1[x]=1;
+                                    if(spr1[d]==0) {
+                                        los1[i][d]= poziom1[i][j];
+                                        spr1[d]=1;
                                         licznik++;
                                         q=false;
                                     }//if
-                                    else x=r.nextInt(los1[i].length);   
+                                    else d=r.nextInt(los1[i].length);   
                                 }//while          
                         }//for
                     }//for
-                                System.out.println(los1[0][0]+los1[0][1]+los1[0][2]);
-                                System.out.println(los1[1][0]+los1[1][1]+los1[1][2]);
-                                System.out.println(los1[2][0]+los1[2][1]+los1[2][2]); 
+                        System.out.println("slowo "+los1[0][0]+los1[0][1]+los1[0][2]);
+                        System.out.println("slowo "+los1[1][0]+los1[1][1]+los1[1][2]);
+                        System.out.println("slowo "+los1[2][0]+los1[2][1]+los1[2][2]);
                     }//case;
                     break;
                 
@@ -62,19 +56,19 @@ public class Losowanie {
                         spr2[3]=0;
                         int licznik =0;
                         for(int j=0; j<poziom2[i].length; j++){
-                            x=r.nextInt(los2[i].length);
+                            d=r.nextInt(los2[i].length);
                              boolean q = false;
                             if (licznik != 4)
                                 q = true;
 
                             while(q){
-                                    if(spr2[x]==0) {
-                                        los2[i][x]= poziom2[i][j];
-                                        spr2[x]=1;
+                                    if(spr2[d]==0) {
+                                        los2[i][d]= poziom2[i][j];
+                                        spr2[d]=1;
                                         licznik++;
                                         q=false;
                                     }//if
-                                    else x=r.nextInt(los2[i].length);   
+                                    else d=r.nextInt(los2[i].length);   
                                 }//while          
                         }//for
                     }//for
@@ -93,19 +87,19 @@ public class Losowanie {
                         spr3[4]=0;
                         int licznik =0;
                         for(int j=0; j<poziom3[i].length; j++){
-                            x=r.nextInt(los3[i].length);
+                            d=r.nextInt(los3[i].length);
                              boolean q = false;
                             if (licznik != 5)
                                 q = true;
 
                             while(q){
-                                    if(spr3[x]==0) {
-                                        los3[i][x]= poziom3[i][j];
-                                        spr3[x]=1;
+                                    if(spr3[d]==0) {
+                                        los3[i][d]= poziom3[i][j];
+                                        spr3[d]=1;
                                         licznik++;
                                         q=false;
                                     }//if
-                                    else x=r.nextInt(los3[i].length);   
+                                    else d=r.nextInt(los3[i].length);   
                                 }//while          
                         }//for
                     }//for
@@ -113,9 +107,10 @@ public class Losowanie {
                                 System.out.println(los3[1][0]+los3[1][1]+los3[1][2]+los3[1][3]+los3[1][4]);
                                 System.out.println(los3[2][0]+los3[2][1]+los3[2][2]+los3[2][3]+los3[2][4]);
                 }//case3
-                break;
+                break; 
             default:
                 break;
         }//switch
+        }//for
     }//losujSlowa
 }//class Losowanie
--- RysowaniePanelu.java
+++ RysowaniePanelu.java
@@ -3,13 +3,15 @@ package seriousgame;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
+import static java.lang.Thread.sleep;
 import java.util.Random;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
+
 import javax.swing.JPanel;
+import static seriousgame.Losowanie.losujSlowa;
 import static seriousgame.Parametry.*;
 
 
@@ -20,37 +22,92 @@ public class RysowaniePanelu extends JPanel{
     int wysPaska=250;
     int szerGry;
     int wysGry=1024;
-    
+    public Font czcionka;
+    int poziom=1;
+    int ktoreSlowo=0;
    
     
-    public RysowaniePanelu(){
+     RysowaniePanelu(){
                    
-                    JButton zakonczGre=new JButton("Zakoncz gre");
-                    JButton zasadyGry=new JButton("Zasady gry");
-                    
-                    add(zakonczGre);
-                    add(zasadyGry);
+        czcionka=new Font("Dialog",Font.BOLD,30);
+        los1= new String [3][3];
+        los2= new String [3][4];
+        los3= new String [3][5];
+        
+        
+        spr1= new int[3];
+        spr2= new int[4];
+        spr3= new int[5];
+        
+        kolejnosc= new int[5];
+        
+        losujSlowa();
+        
+       
+
     }
     
     @Override
     public void paintComponent(Graphics gs){
-           int ktoreSlowo=0;
+          
            //int i=0;
             super.paintComponent(gs);
             Graphics2D g=(Graphics2D)gs;
             //this.setBackground(Color.GRAY);
-            g.drawImage(Parametry.tlo,0,0, 1024, 784, null);
+            //g.drawImage(Parametry.tlo,0,0, 1024, 784, null);
             //g.setColor(Color.BLUE);
             //g.drawRect(0,wysOkna-wysPaska,szerOkna,wysPaska);
             g.setColor(new Color(77,182,250));
             g.fillRect(0,wysOkna-wysPaska,szerOkna,wysPaska);
+            g.setColor(Color.BLACK);
+            g.setFont(czcionka);
+            g.drawString("ZAKOŃCZ GRĘ", 260, 700);
+            g.drawString("ZASADY GRY", 560, 700);
+            
+            switch(poziom){
+                case 1: kolejnosc(los1);
+                        break;
+                case 2: kolejnosc(los2);
+                        break;
+                case 3: kolejnosc(los3);
+                        break;
+            }
+            
+            for(int i=0; i<kolejnosc.length; i++){
+                if(kolejnosc[i]!=-1)
+            g.drawImage(Parametry.krople[kolejnosc[i]],200*i, 200, 250, 250,null);
+            }
+            
+            ktoreSlowo++;
+            if(ktoreSlowo == 3){
+                poziom++;
+                ktoreSlowo=0;
+            }
             
             
-            for(int i=0; i<poziom1[ktoreSlowo].length; i++)
-            g.drawImage(Parametry.krople[i],256, 200, 250, 250,null);
             
             
     
     }//paintComponent
+    
+    
+    public void kolejnosc(String[][] los){
+        
+        for(int i=0; i<kolejnosc.length; i++){
+            kolejnosc[i]=-1;   
+        }
+        int h=0;
+        for(int i=0; i<los[ktoreSlowo].length; i++){
+            for(int j=0; j<alfabet.length;j++){
+                if(los[ktoreSlowo][i]==alfabet[j]){
+                  
+                    kolejnosc[h]=j;
+                    h++;
+                }//if
+            }//for
+        }//for
+    }//kolejnosc
+    
+    
     
 }
