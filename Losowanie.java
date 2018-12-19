3f341015c4f25e6b3da2afd0ee0905475f135503
gjuliag <45037132+gjuliag@users.noreply.github.com>
2018-12-19 11:52:36 +0100

    Add files via upload

--- Losowanie.java
+++ Losowanie.java
@@ -12,6 +12,7 @@ public class Losowanie {
         ladujSlowa();
         Random r= new Random();
         int d;//zmienna z przypisana wylosowana liczba
+        //int jak1[]= new int[3];
         
         for(int terazPoziom=1; terazPoziom<4; terazPoziom++){
         switch (terazPoziom) {
@@ -31,6 +32,7 @@ public class Losowanie {
                             while(q){
                                     if(spr1[d]==0) {
                                         los1[i][d]= poziom1[i][j];
+                                        jak1[i][j]= d;
                                         spr1[d]=1;
                                         licznik++;
                                         q=false;
@@ -39,6 +41,14 @@ public class Losowanie {
                                 }//while          
                         }//for
                     }//for
+                        /*
+                        System.out.println(los1[0][0]+" "+los1[0][1]+" "+los1[0][2]);
+                        System.out.println(jak1[0][0]+" "+jak1[0][1]+" "+jak1[0][2]);
+                        System.out.println(los1[1][0]+" "+los1[1][1]+" "+los1[1][2]);
+                        System.out.println(jak1[1][0]+" "+jak1[1][1]+" "+jak1[1][2]);
+                        System.out.println(los1[2][0]+" "+los1[2][1]+" "+los1[2][2]);
+                        System.out.println(jak1[2][0]+" "+jak1[2][1]+" "+jak1[2][2]);
+                        */
                     }//case1;
                     break;
                 
@@ -59,6 +69,7 @@ public class Losowanie {
                             while(q){
                                     if(spr2[d]==0) {
                                         los2[i][d]= poziom2[i][j];
+                                        jak2[i][j]=d;
                                         spr2[d]=1;
                                         licznik++;
                                         q=false;
@@ -67,6 +78,14 @@ public class Losowanie {
                                 }//while          
                         }//for
                     }//for
+                    /*
+                        System.out.println(los2[0][0]+" "+los2[0][1]+" "+los2[0][2]+" "+los2[0][3]);
+                        System.out.println(jak2[0][0]+" "+jak2[0][1]+" "+jak2[0][2]+" "+jak2[0][3]);
+                        System.out.println(los2[1][0]+" "+los2[1][1]+" "+los2[1][2]+" "+los2[1][3]);
+                        System.out.println(jak2[1][0]+" "+jak2[1][1]+" "+jak2[1][2]+" "+jak2[1][3]);
+                        System.out.println(los2[2][0]+" "+los2[2][1]+" "+los2[2][2]+" "+los2[2][3]);
+                        System.out.println(jak2[2][0]+" "+jak2[2][1]+" "+jak2[2][2]+" "+jak2[2][3]);
+                    */
                 }//case2
                 break;
             case 3:
@@ -87,6 +106,7 @@ public class Losowanie {
                             while(q){
                                     if(spr3[d]==0) {
                                         los3[i][d]= poziom3[i][j];
+                                        jak3[i][j]=d;
                                         spr3[d]=1;
                                         licznik++;
                                         q=false;
@@ -95,7 +115,15 @@ public class Losowanie {
                                 }//while          
                         }//for
                     }//for
-                }//case3
+                        
+                        System.out.println(los3[0][0]+" "+los3[0][1]+" "+los3[0][2]+" "+los3[0][3]+" "+los3[0][4]);
+                        System.out.println(jak3[0][0]+" "+jak3[0][1]+" "+jak3[0][2]+" "+jak3[0][3]+" "+jak3[0][4]);
+                        System.out.println(los3[1][0]+" "+los3[1][1]+" "+los3[1][2]+" "+los3[1][3]+" "+los3[1][4]);
+                        System.out.println(jak3[1][0]+" "+jak3[1][1]+" "+jak3[1][2]+" "+jak3[1][3]+" "+jak3[1][4]);
+                        System.out.println(los3[2][0]+" "+los3[2][1]+" "+los3[2][2]+" "+los3[2][3]+" "+los3[2][4]);
+                        System.out.println(jak3[2][0]+" "+jak3[2][1]+" "+jak3[2][2]+" "+jak3[2][3]+" "+jak3[2][4]);
+                        
+            }//case3
                 break; 
             default:
                 break;
